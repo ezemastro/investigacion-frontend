3ee49bad4657a8fed1277a5b7182eb18791ca38a
ezemastro <mastropietroezequiel@gmail.com>
2024-08-18 11:49:07 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home.jsx'
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: Home()
+    }
+  ])
+
   return (
     <>
-      <header>
-        <h1>Investigación sobre Hábitos de Lectura y Conocimiento de Cultura General</h1>
-        <h2>Ezequiel Mastropietro</h2>
-      </header>
-
-      <main>
-        <section className="1">
-          <p>Completa una breve encuesta y mide tu conocimiento de cultura general con una trivia.</p>
-          <button>Comenzar</button>
-        </section>
-        <section className="2">
-          <p>Esta investigación busca explorar la relación entre los hábitos de lectura y el conocimiento de cultura general en alumnos de secundaria.<br />¡Tu participación es clave para comprender mejor este tema! </p>
-        </section>
-      </main>
-
-      <footer>
-        <p>Para mas información puedes contactarme en <a href="mailto:emastropietro@northfield.edu.ar">emastropietro@northfield.edu.ar</a></p>
-      </footer>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <>
+      <header>
+        <h1>Investigación sobre Hábitos de Lectura y Conocimiento de Cultura General</h1>
+        <h2>Ezequiel Mastropietro</h2>
+      </header>
+
+      <main>
+        <section className="1">
+          <p>Completa una breve encuesta y mide tu conocimiento de cultura general con una trivia.</p>
+          <a href='/mail'>Comenzar</a>
+        </section>
+        <section className="2">
+          <p>Esta investigación busca explorar la relación entre los hábitos de lectura y el conocimiento de cultura general en alumnos de secundaria.<br />¡Tu participación es clave para comprender mejor este tema! </p>
+        </section>
+      </main>
+
+      <footer>
+        <p>Para mas información puedes contactarme en <a href="mailto:emastropietro@northfield.edu.ar">emastropietro@northfield.edu.ar</a></p>
+      </footer>
+    </>
+  )
+}
