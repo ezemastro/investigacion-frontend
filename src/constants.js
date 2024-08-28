537ba7025198a1c5e3636a8ccf916f213f3bbeb3
ezemastro <73084681+ezemastro@users.noreply.github.com>
2024-08-28 15:47:52 -0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const BACKEND_URL = import.meta.env.DEV ? 'http://localhost:3000' : 'https://lectura-vs-conocimiento.onrender.com'
-export const AMMOUNT_OF_QUESTIONS = 61
+export const AMMOUNT_OF_QUESTIONS = 16
 export const TIME_PER_QUESTION = 10
